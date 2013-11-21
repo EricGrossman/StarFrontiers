61bbe30263b4da18252b81f96c90fec7b4d1dc2c
Eric Grossman <egrossman@Erics-MacBook-Pro.local>
2013-11-20 22:34:25 -0500

    Initial commit

---
+++ PlayerCharacter.h
@@ -0,0 +1,13 @@
+//
+//  PlayerCharacter.h
+//  StarFrontiers
+//
+//  Created by Eric Grossman on 11/20/13.
+//  Copyright (c) 2013 Eric Grossman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerCharacter : NSObject
+
+@end
---
+++ PlayerCharacter.m
@@ -0,0 +1,13 @@
+//
+//  PlayerCharacter.m
+//  StarFrontiers
+//
+//  Created by Eric Grossman on 11/20/13.
+//  Copyright (c) 2013 Eric Grossman. All rights reserved.
+//
+
+#import "PlayerCharacter.h"
+
+@implementation PlayerCharacter
+
+@end
